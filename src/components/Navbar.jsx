8bee7178754befafe2edb49ae689fb14bee4e897
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-29 18:38:06 +0530

    29.03.2024

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import { appleImg, bagImg, searchImg } from '../utils';
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex w-full screen-max-width">
+    <header className="flex w-full items-center justify-between p-5 sm:px-10">
+      <nav className="screen-max-width flex w-full">
         <img src={appleImg} alt='Apple' width={14} height={18} />
 
         <div className="flex flex-1 justify-center max-sm:hidden">
             {navLists.map((nav, i) => (
-                <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+                <div key={nav} className="cursor-pointer px-5 text-sm text-gray transition-all hover:text-white">
                     {nav}
                 </div>
             ))}
         </div>
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+        <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
             <img src={searchImg} alt='Search' width={18} height={18} />
             <img src={bagImg} alt='Bag' width={18} height={18} />
         </div>
