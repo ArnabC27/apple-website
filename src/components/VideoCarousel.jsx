f45262303ca9bc0a494d743a770571439d19a2f7
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-30 23:47:29 +0530

    30.03.2024

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { hightlightsSlides } from "../constants";
+import gsap from "gsap";
 
 const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  })
+
+  const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
+
+    if(span[videoId]) {
+        let anim = gsap.to(span[videoId], {
+            onUpdate: () => {
+
+            },
+            onComplete: () => {
+                
+            }
+        })
+    }
+  }, [videoId, startPlay])
+
   return (
     <>
       <div className="flex items-center">
         {hightlightsSlides.map((list, i) => (
-            <div key={list.id} id="slider" className="pr-10 sm:pr-20">
-                <div className="video-carousel_container">
-                    <div className="flex-center size-full overflow-hidden rounded-3xl bg-black">
-                        <video id="video" playsInline={true} preload="auto" muted>
-                            <source src={list.video} type="video/mp4" />
-                        </video>
-                    </div>
+          <div key={list.id} id="slider" className="pr-10 sm:pr-20">
+            <div className="video-carousel_container">
+              <div className="flex-center size-full overflow-hidden rounded-3xl bg-black">
+                <video id="video" playsInline={true} preload="auto" muted>
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
 
-                    <div className="absolute left-[5%] top-12 z-10">
-                        {list.textLists.map((text) => (
-                            <p key={text} className="text-xl font-medium md:text-2xl">
-                                {text}
-                            </p>
-                        ))}
-                        <p></p>
-                    </div>
-                </div>
+              <div className="absolute left-[5%] top-12 z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="text-xl font-medium md:text-2xl">
+                    {text}
+                  </p>
+                ))}
+                <p></p>
+              </div>
             </div>
+          </div>
         ))}
       </div>
     </>
