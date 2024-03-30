a5d2acd4c51a3396efd62764f372ccb5409abda6
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-30 23:28:38 +0530

    30.03.2024

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,7 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import React from 'react'
-import { watchImg } from '../utils'
+import { rightImg, watchImg } from '../utils'
 
 const Highlights = () => {
 
@@ -13,20 +13,26 @@ const Highlights = () => {
 
     gsap.to('.link', {
       opacity: 1,
-      y: 0
+      y: 0,
+      duration: 1,
+      stagger: 0.25
     })
   }, [])
 
   return (
     <section id='highlights' className='common-padding h-full w-screen overflow-hidden bg-zinc'>
       <div className='screen-max-width'>
-        <div className='mb-12 w-full items-end justify-between'>
+        <div className='mb-12 w-full items-end justify-between md:flex'>
           <h1 id='title' className='section-heading'>Get the highlights.</h1>
 
           <div className='flex flex-wrap items-end gap-5'>
             <p className='link'>
               Watch the film
-              <image src={watchImg} alt='watch' className='ml-2' />
+              <img src={watchImg} alt='watch' className='ml-2' />
+            </p>
+            <p className='link'>
+              Watch the event
+              <img src={rightImg} alt='right' className='ml-2' />
             </p>
           </div>
         </div>
