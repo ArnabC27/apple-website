15c7cad5944842ce18f472d4be44dcdcb33f03e2
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-30 23:41:28 +0530

    30.03.2024

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { hightlightsSlides } from "../constants";
 
 const VideoCarousel = () => {
   return (
-    <div>VideoCarousel</div>
-  )
-}
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+            <div key={list.id} id="slider" className="pr-10 sm:pr-20">
+                <div className="video-carousel_container">
+                    <div className="flex-center size-full overflow-hidden rounded-3xl bg-black">
+                        <video id="video" playsInline={true} preload="auto" muted>
+                            <source src={list.video} type="video/mp4" />
+                        </video>
+                    </div>
 
-export default VideoCarousel
+                    <div className="absolute left-[5%] top-12 z-10">
+                        {list.textLists.map((text) => (
+                            <p key={text} className="text-xl font-medium md:text-2xl">
+                                {text}
+                            </p>
+                        ))}
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
