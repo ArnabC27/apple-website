107bda4c5288381a1aaf872f3010f15e7cd1e227
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-30 22:09:08 +0530

    30.03.2024

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,27 +9,31 @@ const Hero = () => {
   );
 
   const handleVideoSecSet = () => {
-    if(window.innerWidth < 760) {
-      setVideoSrc(smallHeroVideo)
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
     }
-    else {
-      setVideoSrc(heroVideo)
-    }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('resize', handleVideoSecSet);
+    window.addEventListener("resize", handleVideoSecSet);
 
     return () => {
-      window.removeEventListener('resize', handleVideoSecSet);
-    }
-  })
+      window.removeEventListener("resize", handleVideoSecSet);
+    };
+  });
 
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
       delay: 1.5,
     });
+    gsap.to('#cta', {
+      opacity: 1,
+      y: -50,
+      delay: 2
+    })
   }, []);
 
   return (
@@ -39,10 +43,26 @@ const Hero = () => {
           iPhone 15 Pro
         </p>
         <div className="w-9/12 md:w-10/12">
-          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex translate-y-20 flex-col items-center opacity-0"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
