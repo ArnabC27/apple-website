35eed99e26e5fe8dfe266076c7d8e40413dcaae3
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-31 21:14:27 +0530

    31.03.2024

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import React from "react";
 
 const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+  useGSAP(() => {
+    
+  }, []);
 
-export default Features
+  return (
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+          <h1 id="features_title" className="section-heading">
+            Explore the full story.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
