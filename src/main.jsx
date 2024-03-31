996ea6bd2593f66b8db0ac09bfb16bd5a334250d
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-31 16:55:49 +0530

    31.03.2024

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,10 @@ Sentry.init({
   dsn: "https://e6db77a6e8f941989e6e6900871538f8@o4507005335109632.ingest.us.sentry.io/4507005337665536",
   integrations: [
     Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
     Sentry.replayIntegration({
       maskAllText: false,
       blockAllMedia: false,
